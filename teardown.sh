c5fd52b97c642e94ec9a64124b4dfbde2725f56c
rajivmb <17132180+rajivmb@users.noreply.github.com>
2020-08-24 00:12:58 +1000

    Prompt message changes

--- teardown.sh
+++ teardown.sh
@@ -41,7 +41,7 @@ printf "DIR is %s" ${DIR}
 
 printf "Commencing tear down...\n"
 
-read -t ${inputTimeout} -p "Enter component name [${projectName}], you have ${inputTimeout}s or press <Enter> to accept default: " input
+read -t ${inputTimeout} -p "Enter component name [${projectName}] or press <Enter> to accept default, you have ${inputTimeout}s: " input
 projectName=${input:-$projectName}
 
 stacks=("${projectName}-DEPLOY" "${projectName}")
